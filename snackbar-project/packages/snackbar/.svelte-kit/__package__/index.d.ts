274fda21b4d220d44166ac8c8ad3464ab314a98b
Kyle Donovan <116353225+KyleDonovan69@users.noreply.github.com>
2025-11-30 14:31:40 +0000

    #10 changed to run with 4173, plus folder cleanup

--- snackbar-project/packages/snackbar/.svelte-kit/__package__/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { default as Snackbar } from './Snackbar.svelte';
-export { default as SnackbarContainer } from './SnackbarContainer.svelte';
-export { snackbarStore, snackbar } from './snackbarStore';
-export type { SnackbarConfig } from './snackbarStore';
-export { requestNotificationPermission, sendNotification, isNotificationGranted, getNotificationPermission } from './notifications';
-export { notifyEventCreated, notifyEventUpdated, notifyEventCancelled, notifyEventReminder, notifyCalendarSynced } from './calendarIntegration';
-export type { CalendarEvent } from './calendarIntegration';
--- snackbar-project/packages/snackbar/.svelte-kit/__package__/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Snackbar } from './Snackbar.svelte';
-export { default as SnackbarContainer } from './SnackbarContainer.svelte';
-export { snackbarStore, snackbar } from './snackbarStore';
-// Notification utilities
-export { requestNotificationPermission, sendNotification, isNotificationGranted, getNotificationPermission } from './notifications';
-// Calendar integration helpers
-export { notifyEventCreated, notifyEventUpdated, notifyEventCancelled, notifyEventReminder, notifyCalendarSynced } from './calendarIntegration';
